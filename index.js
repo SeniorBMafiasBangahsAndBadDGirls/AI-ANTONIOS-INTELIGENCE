eb4d44a2007e27b4c09fa447a1ac933290cd9014
Antonio Roschelley ArBough <antonioryarbough@gmail.com>
2025-08-02 11:05:20 -0400

    family tree

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Antonio Roschelle Yarbough’s Rhyming Family Tree</h1>
+      <p>Where rhyme meets legacy. Powered by Antonio Roschelle Ray Sr's ToneBone AI.</p>
+      <img src="/logo.png" alt="ToneBone Logo" style={{ width: '200px' }} />
+    </div>
+  );
+}
